a3951189432f351ce8c3e69113866e813e61f129
Siegfried <65792191+csieg150@users.noreply.github.com>
2020-08-19 10:53:45 -0500

    Model for albums

---
+++ album-project/src/app/models/Album.ts
@@ -0,0 +1,11 @@
+export class Album{
+    // This will be our Album model to catch all data from our backend.
+
+    name: string; // Album's Name
+    art: string; // Cover Art of the album
+    artist: string; // Artist who produced the albums
+    genre: string; // Main genre of music
+    price: number; // Cost of the album
+    // Song list? not sure yet
+    // download Url? not sure yet
+}
